e13b7634256b2f714b8543765072dca99fa04f91
Daryl Concha <concha.daryl@gmail.com>
2017-10-26 17:01:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,6 @@ var playlist = {
   badSuns: "Heart Breaker",
   }
 
-function updatePlaylist() {
-  playlist['Phil Ochs'] = "Here's to the State of Mississippi";
-  return playlist;
-}
+const newSong
+
+updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
